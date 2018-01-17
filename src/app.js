b24860d0d9c83be57993427a9cab6b174bc95bb2
Idan Tabachnik <idantb101@gmail.com>
2018-01-17 14:15:59 +0200

    cteating test for db and server and basic error hadeleling #10

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('hi there');
+  res.json('hi there');
 });
 
 app.use('/posts', postsRoutes);
@@ -16,3 +16,13 @@ app.use('/posts', postsRoutes);
 app.listen(port, () => {
   console.log(`running on port http://localhost:${port}`);
 });
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app; 
---
+++ tests/server_test.js
@@ -0,0 +1,22 @@
+const test = require('tape');
+const request = require('supertest');
+
+const app = require('../src/app');
+
+test('tape is working', t => {
+  t.equal(1, 1, 'one equals one');
+  t.end();
+});
+
+test('GET /', t => {
+  request(app)
+    .get('/')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res) => {
+        console.log('in end');
+        console.log(res.text)
+      t.end();
+      process.exit(0);
+    });
+});
