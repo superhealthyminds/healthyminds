53097c60b8f0b931c12f94b336197311deb04a6a
Idan Tabachnik <idantb101@gmail.com>
2018-01-18 11:56:56 +0200

    fixing test #10

--- tests/db_test.js
+++ tests/db_test.js
@@ -3,21 +3,18 @@ const tape = require('tape');
 const runDbBuild = require('../src/db/db_buildTest');
 const postQueries = require('../src/db/queries/postQueries');
 
-tape('tape is working', t => {
-  t.equal(1, 1, 'One equals one');
-  t.end();
-});
-
 tape('testing post request', t => {
-  const expected = [{
-    id: 1,
-    type: 'story',
-    title: 'My epic party',
-    content: 'Hey, all invited',
-    commentsid: 1,
-    time_stamp: new Date('20017-12-16 06:00:00'),
-    tagids: 1
-  }];
+  const expected = [
+    {
+      id: 1,
+      type: 'story',
+      title: 'My epic party',
+      content: 'Hey, all invited',
+      commentsid: 1,
+      time_stamp: new Date('20017-12-16 06:00:00'),
+      tagids: 1
+    }
+  ];
   runDbBuild((err, response) => {
     postQueries.getHomePagePosts((err, res) => {
       if (err) {
--- tests/server_test.js
+++ tests/server_test.js
@@ -9,10 +9,37 @@ test('Testing empty root get request - / ', t => {
     .expect('Content-Type', /json/)
     .expect(200)
     .end((err, res) => {
-        console.log('in end');
-        console.log(res.text)
+      console.log(res.text);
+      t.ok(res.text);
       t.end();
-      process.exit(0);
     });
 });
 
+test('Testing empty root get request - / ', t => {
+  const expectedThings = [
+    {
+      id: 1,
+      type: 'story',
+      title: 'My epic party',
+      content: 'Hey, all invited',
+      commentsid: 1,
+      time_stamp: new Date('20017-12-16 06:00:00'),
+      tagids: 1
+    }
+  ];
+  request(app)
+    .get('/posts/homePagePosts')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res) => {
+      t.ok(res.text);
+      let actualThings = res.body;
+      actualThings[0].time_stamp = new Date(actualThings[0].time_stamp);
+      t.same(actualThings, expectedThings, 'should get the only post');
+      t.end();
+    });
+});
+
+test.onFinish(() => {
+  process.exit(0);
+});
