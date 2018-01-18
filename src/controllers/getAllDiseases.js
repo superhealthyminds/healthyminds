0d4df87180b062fa8ea8eef0ee40378a26edf8ce
Amir Khoury <amirkhourykh@gmail.com>
2018-01-18 16:15:56 +0200

    Changed throw error to console.log(error) (((for now))).

--- src/controllers/getAllDiseases.js
+++ src/controllers/getAllDiseases.js
@@ -3,7 +3,8 @@ const diseasesQueries = require('../db/queries/diseasesQueries');
 module.exports = (req, res) => {
   diseasesQueries.getAllDiseases((err, Diseases) => {
     if (err) {
-      throw err;
+      // eslint-disable-next-line no-console
+      console.log(err); // TODO: Error handling
     } else {
       res.json({ Diseases });
     }
--- src/db/db_build.js
+++ src/db/db_build.js
@@ -3,7 +3,8 @@ const connect = require('./db_connections');
 const buildScript = fs.readFileSync(`${__dirname}/db_build.sql`, 'utf8');
 connect.query(buildScript, (err) => { // TODO: Error handling
   if (err) {
-    throw err;
+    // eslint-disable-next-line no-console
+    console.log(err); // TODO: Error handling
   }
   connect.end();
 });
