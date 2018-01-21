466eb91c960bfd9044b2c0acf44afea0339ea3a9
Amir Khoury <amirkhourykh@gmail.com>
2018-01-21 11:40:07 +0200

    Changed quries singlequates to backticks.

--- src/db/queries/chatRoomMessagesQueries.js
+++ src/db/queries/chatRoomMessagesQueries.js
@@ -2,7 +2,11 @@ const connect = require('../db_connections.js');
 
 const getChatRoomMessages = cb => {
   connect.query(
-    'SELECT * FROM messages INNER JOIN chatRooms ON (messages.chatRoomId = chatRooms.id) order by time_stamp',
+    `SELECT *
+    FROM messages
+     INNER JOIN chatRooms ON
+     (messages.chatRoomId = chatRooms.id)
+     order by time_stamp`,
 
     (err, res) => {
       cb(err, res.rows);
