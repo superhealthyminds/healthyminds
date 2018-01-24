f14c9c8e5329053d6ce180f6d6e13089e85ba7e9
Idan Tabachnik <idantb101@gmail.com>
2018-01-24 16:47:06 +0200

    fixing pr comments

--- app/src/app.js
+++ app/src/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('hi there');
+  res.send('sanity check');
 });
 
 app.set('views', path.join(__dirname, 'views'));
